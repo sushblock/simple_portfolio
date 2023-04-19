2e50a53c22a9d59ee704df58d0b0697a0f1b2cb9
sushblock <sushilstudy2013@gmail.com>
2023-04-20 01:39:13 +0530

    modified:   src/assets/image2.jpg modified:   src/components/ProjectDisplay.js modified:   src/data/data.js

--- src/components/ProjectDisplay.js
+++ src/components/ProjectDisplay.js
@@ -3,6 +3,7 @@ import "../styles/ProjectDisplay.css";
 import { useParams } from "react-router-dom";
 import { projectList } from "../data/data";
 import { BsGithub } from "react-icons/bs";
+import {CgWebsite} from "react-icons/cg";
 
 const ProjectDisplay = () => {
   const { id } = useParams();
@@ -13,17 +14,28 @@ const ProjectDisplay = () => {
     <div className="project">
       <h1>{project.projName}</h1>
       <img src={project.image} alt={project.projName} />
-      <p><h2>Reason</h2>{project.reason}</p>
-      <p><h2>Tools</h2>{project.tools}</p>
-      <p><h2>Platform</h2>{project.platform}</p>
-      <p><h2>Description</h2>{project.description}</p>
-      <a
-            href={project.github_url}
-            target="_blank"
-            rel="noreferrer"
-          >
-            <BsGithub />
-          </a>
+      <p>
+        <h2>Reason</h2>
+        {project.reason}
+      </p>
+      <p>
+        <h2>Tools</h2>
+        {project.tools}
+      </p>
+      <p>
+        <h2>Platform</h2>
+        {project.platform}
+      </p>
+      <p>
+        <h2>Description</h2>
+        {project.description}
+      </p>
+      <a href={project.github_url} target="_blank" rel="noreferrer">
+        <BsGithub />
+      </a>
+      <a href={project.website_url} target="_blank" rel="noreferrer">
+        <CgWebsite />
+      </a>
     </div>
   );
 };
