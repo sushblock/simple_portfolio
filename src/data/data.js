686b55c572b07c1526779e3888a45ab646e2990c
sushblock <sushilstudy2013@gmail.com>
2023-08-11 12:52:28 +0530

    image import deleted

--- src/data/data.js
+++ src/data/data.js
@@ -1,5 +1,4 @@
 import image1 from "../assets/image1.png";
-import image2 from "../assets/image2.png";
 import image3 from "../assets/image3.png"
 
 const userMain = { name: "Sushil Kumar", city: "Bangalore", country: "India" };
